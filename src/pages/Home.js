d25850f9b78f9b2888e80aa2d90ff611fba9f402
OutcastR97 <sadman.rashiq.dbz@gmail.com>
2023-05-17 00:19:46 +0600

    home-page-structure

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,23 @@
-import React from 'react';
+import React, { useContext } from "react";
+import Product from "../components/Product";
+import { ProductContext } from "../contexts/ProductContext";
 
 const Home = () => {
-  return <div>Homepage</div>;
+  //get products from product context
+  const { products } = useContext(ProductContext);
+  return (
+    <div>
+      <section className="py-16">
+        <div className="container mx-auto">
+          <div className="grid grid-col-1 md:grid-cols-2 lg:grid-cols-4 xl-grid-cols-5 gap-[30px] max-w-sm mx-auto md:max-w-none md:mx-0">
+            {products.map((product) => {
+              return <Product product={product} key={product.id} />;
+            })}
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 };
 
 export default Home;
